61a2e788588bf787a8bfd158f5172105b0182f48
Masato Nagashima <longisland.1016@gmail.com>
2019-10-24 23:11:02 +0900

    FABをクリックすると音声認識が立ち上がるようにした

--- app/src/main/java/com/example/twist4english/PlayActivity.kt
+++ app/src/main/java/com/example/twist4english/PlayActivity.kt
@@ -1,11 +1,16 @@
 package com.example.twist4english
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+
+//音声認識のActivityのrequestCode
+private const val REQUEST_SPEECH_RECOGNIZER = 3000
 
 const val TONGUE_TWISTER = "TONGUE_TWISTER"
 
@@ -21,6 +26,16 @@ class PlayActivity : AppCompatActivity() {
         mPager = findViewById<OneWaySwipePager>(R.id.pager).apply {
             this.adapter = TongueTwisterSlidePagerAdapter(supportFragmentManager)
             setDirection(SwipeDirection.LEFT)
+        }
+
+        fab.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, tongueTwisters[mPager.currentItem])
+            startActivityForResult(intent, REQUEST_SPEECH_RECOGNIZER)
         }
     }
 }
